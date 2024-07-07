4890cbb4516e4f424ac256192b7f2f3c4df92a46
MukeshKumar2103 <mukesh2132000@gmail.com>
2024-07-07 10:13:12 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
-import React from "react"
-import { Route, Routes } from "react-router-dom"
-import Contact from "./Pages/Contact/Contact"
-import Works from "./Pages/Works/Works"
-import Home from "./Pages/Home/Home"
-import About from "./Pages/About/About"
-import Services from "./Pages/Services/Services"
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Contact from './Pages/Contact/Contact';
+import Works from './Pages/Works/Works';
+import Home from './Pages/Home/Home';
+import About from './Pages/About/About';
+import Services from './Pages/Services/Services';
 
 const App = () => {
   return (
     <div className='scroll-smooth select-none font-quicksand  bg-stone-100'>
       <Routes>
         <Route path='/' element={<Home />} />
+
         <Route path='/about' element={<About />} />
         <Route path='/works' element={<Works />} />
         <Route path='/services' element={<Services />} />
         <Route path='/contact' element={<Contact />} />
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
